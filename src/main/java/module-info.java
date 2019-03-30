ac50bf3af7f7a02ae860f7a456aa00fe84235bae
Leejjon <leejjon@gmail.com>
2019-03-30 19:21:17 +0100

    Fucking works.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module mymodule {
     requires java.ws.rs;
     requires java.xml.bind;
-
+    requires java.json.bind;
     requires grizzly.http.server;
+//    requires jersey.media.jaxb;
+    requires com.sun.xml.bind;
     requires jersey.container.grizzly2.http;
     requires jersey.server;
     requires jersey.common;
